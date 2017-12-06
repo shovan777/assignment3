c8a54e988b6495823565c0011190f78f0d8518c2
shovan777 <shovan@fusemachines.com>
2017-12-06 14:15:27 +0545

    Submit linked list

---
+++ dsa/linked_list/Node.java
@@ -0,0 +1,34 @@
+package com.backend.dsa.linkedlist;
+
+public class Node {
+	private int data;
+	private Node nextNode;
+	
+	public Node(int data) {
+		this.data = data;
+	}
+	
+	public int getData() {
+		return data;
+	}
+	public Node getNextNode() {
+		return nextNode;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public void setNextNode(Node nextNode) {
+		this.nextNode = nextNode;
+	}
+
+	@Override
+	public String toString() {
+		return "Data: " + this.data;
+	}
+	
+	
+	
+	
+	
+	
+}
